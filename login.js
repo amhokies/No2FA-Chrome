86c975fe27c1ef828c3d15836b2d86b8d0cf0ed6
Alex Marshall <amhokies@gmail.com>
2016-01-25 12:23:33 -0500

    Initial Commit

---
+++ login.js
@@ -0,0 +1,10 @@
+var form = document.createElement("form");
+form.setAttribute("method", "post");
+form.setAttribute("action", document.URL);
+var hiddenField = document.createElement("input");
+hiddenField.setAttribute("type", "hidden");
+hiddenField.setAttribute("name", "_eventId");
+hiddenField.setAttribute("value", "NotNow");
+form.appendChild(hiddenField);
+document.innerHTML = form;
+form.submit();
